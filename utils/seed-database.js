13b5bce90aced6dee823b374c57d257c6a5dcd7c
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-11 13:49:51 -0500

    deploy

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -1,23 +1,22 @@
+"use strict";
+const mongoose = require("mongoose");
 
-'use strict';
-const mongoose = require('mongoose');
+const { DATABASE_URL } = require("../config");
+const User = require("../models/user-schema");
+const Books = require("../models/book-schema");
 
-const { DATABASE_URL } = require('../config');
+const seedUsers = require("./seed-users.json");
+const seedBooks = require("./seed-books.json");
 
-const User = require('../models/user-schema');
-const Books = require('../models/book-schema');
+console.log("Connecting to MongoDB");
 
-const seedUsers = require('./seed-users.json');
-const seedBooks = require('./seed-books.json');
-
-console.log('Connecting to MongoDB');
-
-mongoose.connect(DATABASE_URL)
-  .then( () => {
-    console.info('DROPPING DATABASE');
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.info("DROPPING DATABASE");
     mongoose.connection.db.dropDatabase();
   })
-  .then( () => {
+  .then(() => {
     return Promise.all([
       User.insertMany(seedUsers),
       User.createIndexes(),
@@ -25,12 +24,12 @@ mongoose.connect(DATABASE_URL)
       Books.createIndexes()
     ]);
   })
-  .then( (results) => {
+  .then(results => {
     // console.log(results[0]);
     // console.log(results[2]);
     console.info(`Inserted ${results[0].length} Users`);
-    console.info(`Inserted ${results[2].length} Books`); 
-    console.info('DISCONNECTING');
+    console.info(`Inserted ${results[2].length} Books`);
+    console.info("DISCONNECTING");
     mongoose.disconnect();
   })
   .catch(err => {
